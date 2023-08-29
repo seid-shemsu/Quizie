508cf4dcf0684aee3b770fb1c2d04b7740b1de9f
ZiadeWeld <ziade.w@keplerlab.io>
2023-08-29 16:45:37 +0300

    test

--- app/src/main/java/com/seid/quizie/MainActivity.java
+++ app/src/main/java/com/seid/quizie/MainActivity.java
@@ -1,6 +1,7 @@
 package com.seid.quizie;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -33,11 +34,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+        Log.e("key", BuildConfig.API_KEY);
     }
-
-
-
-
-
-
 }
