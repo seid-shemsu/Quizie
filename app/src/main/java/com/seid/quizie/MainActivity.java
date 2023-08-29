81537dbad3fea4afcbcc690bbd895d1003058e0d
ZiadeWeld <ziade.w@keplerlab.io>
2023-08-29 14:47:44 +0300

    sample commit

--- app/src/main/java/com/seid/quizie/MainActivity.java
+++ app/src/main/java/com/seid/quizie/MainActivity.java
@@ -34,10 +34,4 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
-
-
-
-
-
-
 }
